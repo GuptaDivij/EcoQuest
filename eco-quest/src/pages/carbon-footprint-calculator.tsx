5575a8acf1379bc84c14289d4acb3a509ad2b5f6
pham-t-alex <pham.t.alex@gmail.com>
2024-10-28 19:31:46 -0700

    Revert "basic calculator functionality to build off of later"
    This reverts commit 586b5beeeee582d0c33912221d08de642270bdfd.

--- eco-quest/src/pages/carbon-footprint-calculator.tsx
+++ eco-quest/src/pages/carbon-footprint-calculator.tsx
@@ -4,38 +4,9 @@ import {Input, Button} from "@nextui-org/react";
 
 export function CarbonFootprintCalculator() {
     const [hasCar, setHasCar] = useState(false);
-    const [calculated, setCalculated] = useState(false);
-
-    // change to correct values
-    const [weights, setWeights] = useState(new Map([
-        ['Water', 1],
-        ['Gas', 1],
-        ['Electricity', 1],
-        ['Cars', 1],
-        ['Gasoline', 1]
-    ]));
-    
-    const [water, setWater] = useState(0);
-    const [gas, setGas] = useState(0);
-    const [electricity, setElectricity] = useState(0);
-    const [cars, setCars] = useState(0);
-    const [gasoline, setGasoline] = useState(0);
-
-    const [footprint, setFootprint] = useState(0);
 
     const handleCar = () => {
         setHasCar(true);
-    }
-
-    const calculate = () => {
-        let value = 0;
-        value += (weights.get('Water') ?? 0) * water;
-        value += (weights.get('Gas') ?? 0) * gas;
-        value += (weights.get('Electricity') ?? 0) * electricity;
-        value += (weights.get('Cars') ?? 0) * cars;
-        value += (weights.get('Gasoline') ?? 0) * gasoline;
-        setFootprint(value);
-        setCalculated(true);
     }
 
     return (
@@ -43,11 +14,11 @@ export function CarbonFootprintCalculator() {
             <Card>
                 <CardBody>
                     <p>Provide the number of gallons of water consumed in your household from your latest water bill:</p>
-                    <Input placeholder="x gallons" onChange={e => setWater(Number(e.target.value))}></Input>
+                    <Input placeholder="x gallons"></Input>
                     <p>Provide the number of gallons of gas consumed in your household from your latest gas bill:</p>
-                    <Input placeholder="x gallons" onChange={e => setGas(Number(e.target.value))}></Input>
+                    <Input placeholder="x gallons"></Input>
                     <p>Provide the number of kilowatt-hours of electricity consumed in your household from your latest electricity bill:</p>
-                    <Input placeholder="x kilowatt-hours" onChange={e => setElectricity(Number(e.target.value))}></Input>
+                    <Input placeholder="x kilowatt-hours"></Input>
 
                     <div className="flex">
                         <p>Do you have a car?</p>
@@ -57,7 +28,7 @@ export function CarbonFootprintCalculator() {
                     {hasCar ? (
                         <div>
                             <p>How many cars do you have?</p>
-                            <Input placeholder="x cars" onChange={e => setCars(Number(e.target.value))}></Input>
+                            <Input placeholder="x cars"></Input>
                         </div>
                     ):
                         <div></div>
@@ -71,21 +42,10 @@ export function CarbonFootprintCalculator() {
                         <Button>Hybrid</Button>
                         <Button>Electric</Button>
                     </div>
-                    <p>How many gallons of gasoline did you fill in the tank of this car this week?</p>
-                    <Input placeholder="x gallons" onChange={e => setGasoline(Number(e.target.value))}></Input>
-                    <br></br>
-                    <Button variant="shadow" color="success" onClick={calculate}>Submit</Button>
+                    <p>How many gallons of gasoline di you fill in the tank of this car this week?</p>
+                    <Input placeholder="x cars"></Input>
                 </CardBody>
             </Card>
-            {calculated ? (
-                <Card>
-                    <CardBody>
-                        <p>Carbon Footprint: {footprint}</p>
-                    </CardBody>
-                </Card>
-            ):
-                <div></div>
-            }
         </>
         // <h1>This is the carbon footprint calculator page</h1>
     )
